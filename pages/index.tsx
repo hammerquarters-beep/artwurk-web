29213badaa3eacc4a60ab61b42572ba0b0ae3e71
hammerquarters-beep <hammer.quarters@gmail.com>
2026-04-12 11:00:39 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      background: "#000",
+      color: "#fff",
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      ARTWURK
+    </div>
+  );
+}
