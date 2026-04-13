c796363ae02e8298e95a925e2e3c6ff45770f2e6
hammerquarters-beep <hammer.quarters@gmail.com>
2026-04-12 23:52:20 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         background: "#000",
         color: "#fff",
         minHeight: "100vh",
-        padding: "40px",
+        padding: "60px 40px",
         fontFamily: "serif",
       }}
     >
@@ -13,8 +13,8 @@ export default function Home() {
         style={{
           textAlign: "center",
           fontSize: "48px",
-          marginBottom: "60px",
-          letterSpacing: "4px",
+          marginBottom: "80px",
+          letterSpacing: "6px",
         }}
       >
         ARTWURK
@@ -22,15 +22,24 @@ export default function Home() {
 
       <div
         style={{
+          maxWidth: "1200px",
+          margin: "0 auto",
           display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-          gap: "20px",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gap: "30px",
         }}
       >
-        <img src="/artwork/art1.jpg" alt="Art 1" style={{ width: "100%" }} />
-        <img src="/artwork/art2.jpg" alt="Art 2" style={{ width: "100%" }} />
-        <img src="/artwork/art3.jpg" alt="Art 3" style={{ width: "100%" }} />
+        <img src="/artwork/art1.jpg" style={imageStyle} />
+        <img src="/artwork/art2.jpg" style={imageStyle} />
+        <img src="/artwork/art3.jpg" style={imageStyle} />
       </div>
     </div>
   );
 }
+
+const imageStyle = {
+  width: "100%",
+  height: "400px",
+  objectFit: "cover",
+  borderRadius: "4px",
+};
